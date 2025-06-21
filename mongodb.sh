4eb445a57647997f4d2003448889d1fcd4f4357b
anil kumar <akthinq@gmailcom>
2025-06-21 16:49:01 +0530

    practice-roboshop

---
+++ mongodb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+START_TIME = $(date +%s)
+
+# variables
+RED="\e[31m"
+GREEN="\e[32m"
+BLUE="\e[33m"
+YEL="\e[34m"
+RESET="\e[0m"
+
+ROOT_ID=$(id -u)   #to find ID
+
+LOGS_FOLDER="/var/logs/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE= "${LOGS_FOLDER}/${SCRIPT_NAME}.log
+
+echo $LOG_FILE
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started and executing at: $(date)" | tee -a $LOG_FILE
+
+if[$ROOT_ID -ne 0]
+then
+    echo -e "$RED ERROR:: run the script with root access $RESET" | tee -a $LOG_FILE
+else
+    echo -e "$GREEN Script is running... no issues $RESET" | tee -a $LOG_FILE
+fi
+
